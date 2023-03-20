48bc5681593e476aee132ae24bb64edb569a10ef
ariefwl <ariefwl79@gmail.com>
2023-03-20 13:56:09 +0700

    Commit Kedua

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ada kesalahan !',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('auth_user')->plainTextToken;
+        $success['name'] = $user->name;
+        return response()->json([
+            'success' => true,
+            'message' => 'Register berhasil !',
+            'data' => $success
+        ]);
+    }
+}
