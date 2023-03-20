cfac9a404f06b36c56ccd2b6fdaa71bbf90bea8c
ariefwl <ariefwl79@gmail.com>
2023-03-20 20:47:35 +0700

    Commit Ketiga

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -30,12 +31,26 @@ class AuthController extends Controller
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
 
-        $success['token'] = $user->createToken('auth_user')->plainTextToken;
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
         $success['name'] = $user->name;
         return response()->json([
             'success' => true,
             'message' => 'Register berhasil !',
             'data' => $success
         ]);
+    }
+
+    public function login(Request $request)
+    {
+        // if (Auth::attempt([
+        //     'email' => $request['email'],
+        //     'password' => $request['password']
+        // ]))
+        // {
+        //     $auth = Auth::user();
+        //     $success['token'] = $auth->createToken('auth_token')->plainTextToken;
+        // } else {
+
+        // }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
