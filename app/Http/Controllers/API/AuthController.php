56bd5917dd7f16d16f306a48427a011064eafb9f
ariefwl <ariefwl79@gmail.com>
2023-03-23 15:55:46 +0700

    menambahkan function list user

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,6 +10,14 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    public function list()
+    {
+        $user = User::all();
+        return response()->json([
+            'data' => $user
+        ]);
+    }
+
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
